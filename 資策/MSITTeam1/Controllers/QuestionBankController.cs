ddddf03a0add14bf04a3f134dc0fd60d5e5c3469
朱思穎 <chusiyin9@gmail.com>
2022-03-18 11:52:58 +0800

    更新資料庫模型

---
+++ 資策/MSITTeam1/Controllers/QuestionBankController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSITTeam1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSITTeam1.Controllers
+{
+	public class QuestionBankController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult List()
+		{
+			IEnumerable<TChoiceQuestion> ques = from q in (new helloContext()).TChoiceQuestions
+																			 select q;			
+
+			return View(ques);
+		}
+	}
+}
---
+++ 資策/MSITTeam1/Models/TSubmittedAnswer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MSITTeam1.Models
+{
+    public partial class TSubmittedAnswer
+    {
+        public int FSubmitId { get; set; }
+        public string FMemberAccount { get; set; }
+        public string FSubjectId { get; set; }
+        public string FQuestionId { get; set; }
+        public string FSubmitAnswer { get; set; }
+    }
+}
