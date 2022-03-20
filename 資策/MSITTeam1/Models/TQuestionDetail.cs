75ee626921bb37523e9b02e02e20fa20c53d81ad
陳泓里 <awderfde123@gmail.com>
2022-03-20 19:52:31 +0800

    SQL DATAUP

--- 資策/MSITTeam1/Models/TQuestionDetail.cs
+++ 資策/MSITTeam1/Models/TQuestionDetail.cs
@@ -8,8 +8,8 @@ namespace MSITTeam1.Models
     public partial class TQuestionDetail
     {
         public string FSubjectId { get; set; }
-        public string FQuestionId { get; set; }
-        public string FDetail { get; set; }
+        public int FQuestionId { get; set; }
+        public string FChoice { get; set; }
         public byte[] FImage { get; set; }
         public int FCorrectAnswer { get; set; }
     }
--- 資策/MSITTeam1/Models/TSubmittedAnswer.cs
+++ 資策/MSITTeam1/Models/TSubmittedAnswer.cs
@@ -7,10 +7,11 @@ namespace MSITTeam1.Models
 {
     public partial class TSubmittedAnswer
     {
-        public int FSubmitId { get; set; }
+        public int FSn { get; set; }
         public string FMemberAccount { get; set; }
         public string FSubjectId { get; set; }
         public string FQuestionId { get; set; }
         public string FSubmitAnswer { get; set; }
+        public string FCorrectAnswer { get; set; }
     }
 }
