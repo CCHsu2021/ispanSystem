898a9a6b0427dd1280a22bac12907e1bdc2c8a5d
陳泓里 <awderfde123@gmail.com>
2022-03-25 20:31:35 +0800

    後臺加入SQL

---
+++ 資策/MSITTeam1Admin/Models/TClassGrade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MSITTeam1Admin.Models
+{
+    public partial class TClassGrade
+    {
+        public string FAccount { get; set; }
+        public string FMemberName { get; set; }
+        public int? FBeforeClassGrade { get; set; }
+        public int? FAfterClassGrade { get; set; }
+        public int? FTestPaperId { get; set; }
+        public int? FIscompany { get; set; }
+    }
+}
---
+++ 資策/MSITTeam1Admin/Models/TQuestionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MSITTeam1Admin.Models
+{
+    public partial class TQuestionType
+    {
+        public int QuestionTypeId { get; set; }
+        public string QuestionType { get; set; }
+    }
+}
