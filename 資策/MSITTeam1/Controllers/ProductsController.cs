5e328677e390efc333310356b0d97584f9292ffb
CCHsu <nicohsu.4co@gmail.com>
2022-03-30 09:04:32 +0800

    Product

---
+++ 資策/MSITTeam1/Controllers/ProductsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MSITTeam1.Models;
+
+namespace MSITTeam1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly helloContext _context;
+
+        public ProductsController(helloContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TProducts.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tProduct = await _context.TProducts
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (tProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(tProduct);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,Type,Name,Price,Cost,ImgPath,Barcode")] TProduct tProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tProduct);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tProduct);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tProduct = await _context.TProducts.FindAsync(id);
+            if (tProduct == null)
+            {
+                return NotFound();
+            }
+            return View(tProduct);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("ProductId,Type,Name,Price,Cost,ImgPath,Barcode")] TProduct tProduct)
+        {
+            if (id != tProduct.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tProduct);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TProductExists(tProduct.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tProduct);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tProduct = await _context.TProducts
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (tProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(tProduct);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var tProduct = await _context.TProducts.FindAsync(id);
+            _context.TProducts.Remove(tProduct);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TProductExists(string id)
+        {
+            return _context.TProducts.Any(e => e.ProductId == id);
+        }
+    }
+}
---
+++ 資策/MSITTeam1/ViewModels/CProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using MSITTeam1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSITTeam1.ViewModels
+{
+    public class CProductViewModel
+    {
+        private TProduct _product = null;
+        public CProductViewModel()
+        {
+            _product = new TProduct();
+        }
+        public TProduct prodcut
+        {
+            get { return _product; }
+            set { _product = value; }
+        }
+        public string ProductId { get; set; }
+        public int? Type { get; set; }
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? Cost { get; set; }
+        public IFormFile ImgPath { get; set; }
+        public int? Barcode { get; set; }
+    }
+}
