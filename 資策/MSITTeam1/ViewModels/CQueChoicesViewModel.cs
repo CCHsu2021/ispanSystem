2d168778e7f7fa2a6984be9c621f90c6df63f877
朱思穎 <chusing918@qq.com>
2022-04-01 16:31:28 +0800

    暫存

---
+++ 資策/MSITTeam1/ViewModels/CQueChoicesViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MSITTeam1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSITTeam1.ViewModels
+{
+	public class CQueChoicesViewModel
+	{
+		private TQuestionDetail _choice = null;
+		public TQuestionDetail choice
+		{
+			get { return _choice; }
+			set { _choice = value; }
+		}
+
+		[DisplayName("選項")]
+		public string Vchoice
+		{
+			get { return this.choice.FChoice; }
+			set { this.choice.FChoice = value; }
+		}
+
+		public int VcorrectAnswer
+		{
+			get { return this.choice.FCorrectAnswer; }
+			set { this.choice.FCorrectAnswer = value; }
+		}
+	}
+}
