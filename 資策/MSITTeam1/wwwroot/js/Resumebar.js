74420b28fff7185219523d5230affb0867d21342
邱思誠 <g4125874@gmail.com>
2022-04-03 23:15:54 +0800

    new

--- 資策/MSITTeam1/wwwroot/js/Resumebar.js
+++ 資策/MSITTeam1/wwwroot/js/Resumebar.js
@@ -1,51 +1,75 @@
-$(document).ready(function () {
-    //Initialize tooltips
-    $('.nav-tabs > li a[title]').tooltip();
+var currentSlide = 0,
+    $slideContainer = $('.slide-container'),
+    $slide = $('.slide'),
+    slideCount = $slide.length,
+    animationTime = 300;
 
-    //Wizard
-    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-
-        var $target = $(e.target);
-
-        if ($target.parent().hasClass('disabled')) {
-            return false;
-        }
-    });
-
-    $(".next-step").click(function (e) {
-
-        var $active = $('.wizard .nav-tabs li.active');
-        $active.next().removeClass('disabled');
-        nextTab($active);
-
-    });
-    $(".prev-step").click(function (e) {
-
-        var $active = $('.wizard .nav-tabs li.active');
-        prevTab($active);
-
-    });
-
-    $(".next-step").click(function (e) {
-        $('#form').validator().on('submit', function (e) {
-            if (e.isDefaultPrevented()) {
-                // handle the invalid form...
-                alert("Failed");
-            } else {
-                // everything looks good!
-                alert("Successful!");
-            }
-        });
-    });
-
-
-
-    //End of Doc Ready
-});
-
-function nextTab(elem) {
-    $(elem).next().find('a[data-toggle="tab"]').click();
+function setSlideDimensions() {
+    var windowWidth = $(window).width();
+    $slideContainer.width(windowWidth * slideCount);
+    $slide.width(windowWidth);
 }
-function prevTab(elem) {
-    $(elem).prev().find('a[data-toggle="tab"]').click();
+
+function generatePagination() {
+    var $pagination = $('.pagination');
+    for (var i = 0; i < slideCount; i++) {
+        var $indicator = $('<div>').addClass('indicator'),
+            $progressBarContainer = $('<div>').addClass('progress-bar-container'),
+            $progressBar = $('<div>').addClass('progress-bar'),
+            indicatorTagText = $slide.eq(i).attr('data-tag'),
+            $tag = $('<div>').addClass('tag').text(indicatorTagText);
+        $indicator.append($tag);
+        $progressBarContainer.append($progressBar);
+        $pagination.append($indicator).append($progressBarContainer);
+    }
+    $pagination.find('.indicator').eq(0).addClass('active');
 }
+
+function goToNextSlide() {
+    if (currentSlide >= slideCount - 1) return;
+    var windowWidth = $(window).width();
+    currentSlide++;
+    $slideContainer.animate({
+        left: -(windowWidth * currentSlide)
+    });
+    setActiveIndicator();
+    $('.progress-bar').eq(currentSlide - 1).animate({
+        width: '100%'
+    }, animationTime);
+}
+
+function goToPreviousSlide() {
+    if (currentSlide <= 0) return;
+    var windowWidth = $(window).width();
+    currentSlide--;
+    $slideContainer.animate({
+        left: -(windowWidth * currentSlide)
+    }, animationTime);
+    setActiveIndicator();
+    $('.progress-bar').eq(currentSlide).animate({
+        width: '0%'
+    }, animationTime);
+}
+
+function postitionSlides() {
+    var windowWidth = $(window).width();
+    setSlideDimensions();
+    $slideContainer.css({
+        left: -(windowWidth * currentSlide)
+    }, animationTime);
+}
+
+function setActiveIndicator() {
+    var $indicator = $('.indicator');
+    $indicator.removeClass('active').removeClass('complete');
+    $indicator.eq(currentSlide).addClass('active');
+    for (var i = 0; i < currentSlide; i++) {
+        $indicator.eq(i).addClass('complete');
+    }
+}
+
+setSlideDimensions();
+generatePagination();
+$(window).resize(postitionSlides);
+$('.next').on('click', goToNextSlide);
+$('.previous').on('click', goToPreviousSlide);
