39875931fffe79bd0707bf80b45f6d03788af344
CCHsu <nicohsu.4co@gmail.com>
2022-05-08 17:57:36 +0800

    購買紀錄

--- 資策/MSITTeam1/Models/TPointOrder.cs
+++ 資策/MSITTeam1/Models/TPointOrder.cs
@@ -9,10 +9,10 @@ namespace MSITTeam1.Models
     {
         public string CompanyTaxid { get; set; }
         public string OrderId { get; set; }
-        public string OrderDate { get; set; }
+        public DateTime OrderDate { get; set; }
         public int? BuyPoint { get; set; }
         public string PayMethod { get; set; }
-        public float? Bonus { get; set; }
+        public double? Bonus { get; set; }
         public int? ToTalGetPoint { get; set; }
     }
 }
